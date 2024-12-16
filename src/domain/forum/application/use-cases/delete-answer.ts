2935cfec6563c9a74daecf8c635a02fbc64f87e0
FabioBasagliaVasconcellos <fbcvasconcellos@gmail.com>
2024-12-16 21:36:06 +0000

    Delete Answers

--- src/domain/forum/application/use-cases/delete-answer.ts
+++ src/domain/forum/application/use-cases/delete-answer.ts
@@ -2,6 +2,7 @@ import { Either, left, right } from "src/core/either";
 import { AnswersRepository } from "../repositories/answers-repository";
 import { ResourceNotFoundError } from "src/core/errors/errors/resource-not-found-error";
 import { NotAllowedError } from "src/core/errors/errors/not-allowed-error";
+import { Injectable } from "@nestjs/common";
 
 interface DeleteAnswerUseCaseRequest {
   authorId: string;
@@ -13,6 +14,7 @@ type DeleteAnswerUseCaseResponse = Either<
   null
 >;
 
+@Injectable()
 export class DeleteAnswerUseCase {
   constructor(private answerRepository: AnswersRepository) {}
 
--- src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -56,7 +56,7 @@ export class PrismaAnswersRepository implements AnswersRepository {
   async save(answer: Answer): Promise<void> {
     const data = PrismaAnswerMapper.toPrisma(answer);
 
-    await this.prisma.comment.update({
+    await this.prisma.answer.update({
       where: {
         id: data.id,
       },
