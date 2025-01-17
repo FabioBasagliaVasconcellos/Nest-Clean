3043eb959bddf1ec08ff936800be8cdc9a0984a6
FabioBasagliaVasconcellos <fbcvasconcellos@gmail.com>
2025-01-17 04:20:39 +0000

    Questions with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questio
 import { CreateQuestionUseCase } from "./create-question";
 import { InMemoryQuestionAttachmentsRepository } from "test/repositories/in-memory-question-attachments-repository";
 import { UniqueEntityID } from "src/core/entities/unique-entity-id";
+import exp from "node:constants";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
@@ -38,5 +39,27 @@ describe("Create Question", () => {
       expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
       expect.objectContaining({ attachmentId: new UniqueEntityID("2") }),
     ]);
+  });
+
+  it("should persist attachments when creating a new question", async () => {
+    const result = await sut.execute({
+      authorId: "1",
+      title: "New Question",
+      content: "Question Content",
+      attachmentsIds: ["1", "2"],
+    });
+
+    expect(result.isRight()).toBe(true);
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(2);
+    expect(inMemoryQuestionAttachmentsRepository.items).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID("1"),
+        }),
+        expect.objectContaining({
+          attachmentId: new UniqueEntityID("2"),
+        }),
+      ])
+    );
   });
 });
