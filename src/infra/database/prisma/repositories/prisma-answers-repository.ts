3d8cae8bc0489bb24315b8593c56688466de0a39
FabioBasagliaVasconcellos <fbcvasconcellos@gmail.com>
2025-01-23 00:12:03 +0000

    answers with attachments

--- src/infra/database/prisma/repositories/prisma-answers-repository.ts
+++ src/infra/database/prisma/repositories/prisma-answers-repository.ts
@@ -4,10 +4,14 @@ import { Answer } from "src/domain/forum/enterprise/entities/answer";
 import { PrismaService } from "../prisma.service";
 import { PrismaAnswerMapper } from "../mappers/prisma-answer-mapper";
 import { PaginationParams } from "src/core/repositories/pagination-params";
+import { AnswerAttachmentsRepository } from "src/domain/forum/application/repositories/answer-attachments-repository";
 
 @Injectable()
 export class PrismaAnswersRepository implements AnswersRepository {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private answerAttachmentsRepository: AnswerAttachmentsRepository
+  ) {}
 
   async findById(id: string): Promise<Answer | null> {
     const answer = await this.prisma.answer.findUnique({
@@ -45,22 +49,37 @@ export class PrismaAnswersRepository implements AnswersRepository {
     await this.prisma.answer.create({
       data,
     });
+
+    await this.answerAttachmentsRepository.createMany(
+      answer.attachments.getItems()
+    );
   }
+
+  async save(answer: Answer): Promise<void> {
+    const data = PrismaAnswerMapper.toPrisma(answer);
+
+    await Promise.all([
+      this.prisma.answer.update({
+        where: {
+          id: data.id,
+        },
+        data,
+      }),
+
+      this.answerAttachmentsRepository.createMany(
+        answer.attachments.getNewItems()
+      ),
+      this.answerAttachmentsRepository.deleteMany(
+        answer.attachments.getRemovedItems()
+      ),
+    ]);
+  }
+
   async delete(answer: Answer): Promise<void> {
     await this.prisma.answer.delete({
       where: {
         id: answer.id.toString(),
       },
-    });
-  }
-  async save(answer: Answer): Promise<void> {
-    const data = PrismaAnswerMapper.toPrisma(answer);
-
-    await this.prisma.answer.update({
-      where: {
-        id: data.id,
-      },
-      data,
     });
   }
 }
